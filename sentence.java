31b1e1a974bd37a081ca7a752a8ff238e56a6a71
ftem2002 <91579449+ftem2002@users.noreply.github.com>
2021-12-24 00:32:49 +0300

    Add files via upload

---
+++ sentence.java
@@ -0,0 +1,31 @@
+import javax.swing.JOtionPane;
+
+public class sentence 
+import javax.swing.JOtionPane;
+
+
+{
+
+	public static void main(String[] args) 
+	{
+	String orange , apple ;
+	int orange_count , apples_count , total_fruits_count;
+	orange = JoptionPane.showInputDiolog("enter the number of oranges:");
+	orange_count = Integer.parseInt(orange);
+	
+	apple = Joptionpane.showInputDiolog("enter the number of apples :");
+	apples_count = Integer.parseInt(apple);
+	
+	total_fruits_count = orange_count + apples_count;
+	System.out.println("total_fruits_count" + total_fruits_count );
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
